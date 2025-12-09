5d775bf243f3aaf571f428aa6a5f231ea902636d
MD Sojal Islam <sojolislam576@gmail.com>
2025-12-10 00:57:49 +0600

    Add files via upload
    git admin backend models and urls update

---
+++ admin/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from accounts.models import CustomUser
+from app.models import Job, Course
+
+# Create your views here.
+def admin_dashboard(request):
+    total_users = CustomUser.objects.count()
+    total_active_jobs = Job.objects.filter(is_active=True).count()
+    total_courses = Course.objects.filter(is_active=True).count()
+    recent_users = CustomUser.objects.select_related('user').order_by('-created_at')[:5]
+
+    context = {
+        'total_users': total_users,
+        'total_active_jobs': total_active_jobs,
+        'total_courses': total_courses,
+        'recent_users': recent_users,
+    }
+    return render(request, 'admin_dashboard.html', context)
+
+def admin_user(request):
+    total_users = CustomUser.objects.count()
+    total_active_jobs = Job.objects.filter(is_active=True).count()
+    total_courses = Course.objects.filter(is_active=True).count()
+    recent_users = CustomUser.objects.select_related('user').order_by('-created_at')[:5]
+
+    context = {
+        'total_users': total_users,
+        'total_active_jobs': total_active_jobs,
+        'total_courses': total_courses,
+        'recent_users': recent_users,
+    }
+    return render(request, 'admin_item/admin-item.html',context)
+def course_quiz(request):
+    return render(request, 'quizzes/course_quiz.html')
+
